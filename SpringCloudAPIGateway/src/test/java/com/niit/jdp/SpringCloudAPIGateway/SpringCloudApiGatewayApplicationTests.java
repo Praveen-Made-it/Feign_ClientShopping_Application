e77bfa300d1951620960cff1a3c51ecd9789ddfe
Praveen Kumar <praveen170596@gmail.com>
2022-12-07 17:41:02 +0530

    added SpringCloudApiGatewayApplication

---
+++ SpringCloudAPIGateway/src/test/java/com/niit/jdp/SpringCloudAPIGateway/SpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.SpringCloudAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
