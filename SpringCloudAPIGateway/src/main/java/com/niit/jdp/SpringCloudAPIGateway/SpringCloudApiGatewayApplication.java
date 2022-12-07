ff7db27b13c983983cfb1469693c7c2819123dca
Praveen Kumar <praveen170596@gmail.com>
2022-12-07 19:14:28 +0530

    changes done in SpringCloudApiGatewayApplication

--- SpringCloudAPIGateway/src/main/java/com/niit/jdp/SpringCloudAPIGateway/SpringCloudApiGatewayApplication.java
+++ SpringCloudAPIGateway/src/main/java/com/niit/jdp/SpringCloudAPIGateway/SpringCloudApiGatewayApplication.java
@@ -2,7 +2,12 @@ package com.niit.jdp.SpringCloudAPIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class SpringCloudApiGatewayApplication {
 
@@ -10,4 +15,9 @@ public class SpringCloudApiGatewayApplication {
 		SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator getRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().route(p -> p.path("/api/v1/**").uri("http://customer-service:8087/"))
+				.route(p -> p.path("/api/v2/**").uri("http://authentication-service:8083/")).build();
+	}
 }
